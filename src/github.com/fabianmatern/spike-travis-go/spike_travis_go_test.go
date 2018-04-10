23528c16b1a7b54beceb96b72f063edccab71e03
Fabian Matern <fmatern@thoughtworks.com>
2018-04-10 13:56:57 +0200

    Try to run ginkgo

---
+++ src/github.com/fabianmatern/spike-travis-go/spike_travis_go_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/fabianmatern/spike-travis-go"
+)
+
+var _ = Describe("SpikeTravisGo", func() {
+
+})
